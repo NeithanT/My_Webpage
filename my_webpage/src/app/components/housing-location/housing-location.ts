9ca7259473d1e901bcc2efdf40852b93abd3c9ca
NeithanT <n.vargas.5@estudiantec.cr>
2025-12-22 20:48:54 -0600

    Added basic template example from angular tutorial

---
+++ my_webpage/src/app/components/housing-location/housing-location.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { HousingLocationInfo } from './housing-location-info';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  templateUrl: './housing-location.html',
+  styleUrl: './housing-location.css',
+})
+export class HousingLocation {
+
+  housingLocation = input.required<HousingLocationInfo>();
+}
